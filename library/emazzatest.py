bcf3f648cdb49d2141440314cc9c25259e356bfa
emanuelemazza <emazza@vmware.com>
2016-03-02 08:57:55 +0100

    Munich Day 1

--- library/emazzatest.py
+++ library/emazzatest.py
@@ -1,9 +1,9 @@
 from nsxramlclient.client import NsxClient
-import time
 import argparse
 import ConfigParser
 
-def logical_switch_create(client_session,transport_zone,logical_switch_name,control_plane_mode='UNICAST_MODE'):
+
+def logical_switch_create(client_session, transport_zone, logical_switch_name, control_plane_mode='UNICAST_MODE'):
     # find the objectId of the Scope with the name of the Transport Zone
     vdn_scopes = client_session.read('vdnScopes', 'read')['body']
     vdn_scope_dict_list = [scope_dict for scope_dict in vdn_scopes['vdnScopes'].items()]
@@ -11,7 +11,7 @@ def logical_switch_create(client_session,transport_zone,logical_switch_name,cont
 
     # get a template dict for the lswitch create
     lswitch_create_dict = client_session.extract_resource_body_schema('logicalSwitches', 'create')
-    #client_session.view_body_dict(lswitch_create_dict)
+    # client_session.view_body_dict(lswitch_create_dict)
 
     # fill the details for the new lswitch in the body dict
     lswitch_create_dict['virtualWireCreateSpec']['controlPlaneMode'] = control_plane_mode
@@ -21,7 +21,8 @@ def logical_switch_create(client_session,transport_zone,logical_switch_name,cont
     # create new lswitch
     new_ls = client_session.create('logicalSwitches', uri_parameters={'scopeId': vdn_scope},
                                    request_body_dict=lswitch_create_dict)
-    #client_session.view_response(new_ls)
+    client_session.view_response(new_ls)
+
 
 def main():
     parser = argparse.ArgumentParser(description="nsxv function for logical switch '%(prog)s @params.conf'.",
@@ -53,13 +54,13 @@ def main():
     if args.transport_zone:
         transport_zone = args.transport_zone
     else:
-        transport_zone = config.get('defaults','transport_zone')
+        transport_zone = config.get('defaults', 'transport_zone')
 
-    #print ('test "{}"'.format(transport_zone))
-    session = NsxClient(config.get('nsxraml','nsxraml_file'), config.get('nsxv','nsx_manager'),
-                        config.get('nsxv','nsx_username'), config.get('nsxv','nsx_password'), debug=debug)
+    # print ('test "{}"'.format(transport_zone))
+    session = NsxClient(config.get('nsxraml', 'nsxraml_file'), config.get('nsxv', 'nsx_manager'),
+                        config.get('nsxv', 'nsx_username'), config.get('nsxv', 'nsx_password'), debug=debug)
 
-    logical_switch_create(session,transport_zone,args.name)
+    logical_switch_create(session, transport_zone, args.name)
 
 
 if __name__ == "__main__":
