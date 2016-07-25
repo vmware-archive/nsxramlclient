e323486903eec82da39d03ba06130f4a9ac5e613
yfauser <yfauser@vmware.com>
2016-07-25 16:19:53 -0700

    v2.0.1 Add support for repeating keys in the XML body without nested structure bellow
    - Before v2.0.1 nsxramlclient only supports repeating keys in the XML body if these repeating key themself hold multiple keys bellow them (nesting). This patch adds the support for repeating keys that only hold a values bellow them and no nested structure. This was needed to support the API call to create secondary IP Addresses on ESG router interfaces

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = read('README.rst')
 
 setup(
     name='nsxramlclient',
-    version='2.0.0',
+    version='2.0.1',
     packages=['nsxramlclient'],
     url='http://github.com/vmware/nsxramlclient',
     license='MIT',
