9736e05ac0f3700056d978f51487a04731dc468d
Marko Zagožen <marko.zagozen@gmail.com>
2019-05-06 16:42:10 +0200

    Use six.moves.input for python2 raw_input() replacement.

--- setup.py
+++ setup.py
@@ -29,5 +29,5 @@ setup(
     'Topic :: Software Development :: Libraries',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 2.7'],
-    install_requires=['pyopenssl', 'pyraml-parser>=0.1.3', 'lxml<=4.3.3', 'requests>=2.7.0']
+    install_requires=['pyopenssl', 'pyraml-parser>=0.1.3', 'lxml<=4.3.3', 'requests>=2.7.0', 'six']
 )
